71e767fb1bfbe0be5bb2445e7e6ec934d0b767bf
Josue710 <76234318+Josue710@users.noreply.github.com>
2022-05-02 18:25:38 -0400

    proyeto twitter

---
+++ script.js
@@ -0,0 +1,62 @@
+function Funcion() {
+    location.href = "https://about.twitter.com/es";
+}
+function Funct() {
+    location.href = "https://twitter.com/es/tos";
+}
+function Centro(){
+    location.href = "https://help.twitter.com/es";
+}
+function Condiciones(){
+    location.href = "https://twitter.com/es/tos";
+}
+function Politica(){
+    location.href = "https://twitter.com/es/privacy";
+}
+function Politica_Cook(){
+    location.href = "https://help.twitter.com/es/rules-and-policies/twitter-cookies";
+}
+function Accesibilidad(){
+    location.href = "https://help.twitter.com/es/resources/accessibility";
+}
+function Accesibilidad(){
+    location.href = "https://help.twitter.com/es/resources/accessibility";
+}
+function Info_Anuncios(){
+    location.href = "https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo";
+}
+function Blog(){
+    location.href = "https://blog.twitter.com/";
+}
+function Estado(){
+    location.href = "https://status.twitterstat.us/";
+}
+function Empleos(){
+    location.href = "https://careers.twitter.com/";
+}
+function Recurso(){
+    location.href = "https://about.twitter.com/es/company/brand-resources";
+}
+function Publicidad(){
+    location.href = "https://careers.twitter.com/";
+}
+function Marketing(){
+    location.href = "https://marketing.twitter.com/es";
+}
+function Tw_Empresas(){
+    location.href = "https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=twitterforbusiness";
+}
+function Desarrolladores(){
+    location.href = "https://developer.twitter.com/en";
+}
+function Guia(){
+    location.href = "https://twitter.com/i/directory/profiles";
+}
+function Configuracion(){
+    location.href = "https://twitter.com/settings/account/personalization";
+}
+
+
+
+
+
